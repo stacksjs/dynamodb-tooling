eac6db018cea5aa5fb96ebd86680aa8542d1697d
Chris <chrisbreuer93@gmail.com>
2024-03-17 00:36:30 -0700

    chore: wip

--- src/dynamodb.ts
+++ src/dynamodb.ts
@@ -18,15 +18,39 @@ const JARNAME = 'DynamoDBLocal.jar'
 const runningProcesses: { [port: number]: ChildProcess } = {}
 
 interface LaunchOptions {
+  /**
+   * The port to run the local DynamoDB instance on
+   */
   port: number
+  /**
+   * The path to the database file
+   */
   dbPath?: string
+  /**
+   * Additional arguments to pass to the DynamoDBLocal process
+   */
   additionalArgs?: string[]
+  /**
+   * Whether to log additional information
+   */
   verbose?: boolean
+  /**
+   * Whether to run the process in detached mode
+   */
   detached?: boolean
+  /**
+   * Additional options to pass to the JVM
+   */
   javaOpts?: string
 }
 
 export const dynamoDb = {
+  /**
+   * Launches a new DynamoDBLocal process
+   * @param options LaunchOptions - The options to use when launching the DynamoDBLocal process
+   * @returns {Promise<ChildProcess | undefined>} A promise that resolves to the ChildProcess of the DynamoDBLocal instance.
+   * @throws An error if the process fails to start
+   */
   async launch({ port, dbPath, additionalArgs = [], verbose = false, detached, javaOpts = '' }: LaunchOptions): Promise<ChildProcess | undefined> {
     if (runningProcesses[port])
       return Promise.resolve(runningProcesses[port])
@@ -75,6 +99,11 @@ export const dynamoDb = {
       })
   },
 
+  /**
+   * Stops the DynamoDBLocal process running on the specified port
+   * @param {number} port - The port to stop the DynamoDBLocal process on
+   * @returns {void}
+   */
   stop(port: number): void {
     const process = runningProcesses[port]
     if (process) {
@@ -83,6 +112,11 @@ export const dynamoDb = {
     }
   },
 
+  /**
+   * Stops the specified child process
+   * @param {ChildProcess} child - The child process to stop
+   * @returns {void}
+   */
   stopChild(child: ChildProcess): void {
     if (child.pid) {
       debug('Stopped the child')
@@ -90,11 +124,22 @@ export const dynamoDb = {
     }
   },
 
+  /**
+   * Relaunches the DynamoDBLocal process on the specified port
+   * @param {number} port - The port to run the local DynamoDB instance on
+   * @param {string[]} [args] - Additional arguments to pass to the DynamoDBLocal process
+   * @returns {void}
+   */
   relaunch(port: number, ...args: string[]): void {
     this.stop(port)
     this.launch({ port, additionalArgs: args })
   },
 
+  /**
+   * Configures the installer with the specified configuration
+   * @param {Partial<Config>} conf - The configuration to use
+   * @returns {void}
+   */
   configureInstaller(conf: Partial<Config>): void {
     if (conf.installPath)
       config.installPath = conf.installPath
@@ -103,6 +148,10 @@ export const dynamoDb = {
       config.downloadUrl = conf.downloadUrl
   },
 
+  /**
+   * Installs the DynamoDBLocal instance
+   * @returns {Promise<void>} A promise that resolves when the installation is complete
+   */
   async install(): Promise<void> {
     debug('Checking for DynamoDB-Local in ', config.installPath)
     const mkdir = promisify(fs.mkdir)
