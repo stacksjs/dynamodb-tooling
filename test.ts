39a1dad4d5c764141e361b71b7fe2a121582b804
Chris <chrisbreuer93@gmail.com>
2024-09-30 09:39:07 +0200

    chore: remove eslint comments

--- test.ts
+++ test.ts
@@ -1,14 +1,10 @@
-import { dynamoDb } from './dist'
+import { dynamoDb } from './dist/'
 
-// eslint-disable-next-line no-console
 console.log('Starting DynamoDB on port 8000')
 
 const port = 8000
 // if you want to share with Bun Shell
-// eslint-disable-next-line antfu/no-top-level-await
 const dd = await dynamoDb.launch()
 
-// eslint-disable-next-line no-console
 console.log('dd', dd)
-// eslint-disable-next-line no-console
 console.log(`DynamoDB started on port ${port}`)
