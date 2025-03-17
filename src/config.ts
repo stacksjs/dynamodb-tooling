e0995e71a2635d7628eb393c7930905f1b1630fb
Chris <chrisbreuer93@gmail.com>
2025-03-16 22:16:34 -0700

    chore: use bunfig

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,21 @@
 import type { Config } from './types'
-import { loadConfig } from 'c12'
+import { resolve } from 'node:path'
+import { loadConfig } from 'bunfig'
 
-// Define an async function to load the config
-async function loadDynamoDBConfig(): Promise<Config> {
-  const { config } = (await loadConfig({
-    name: 'dynamodb',
-    defaultConfig: {
-      port: 8000,
-      dbPath: '',
-      detached: false,
-      additionalArgs: ['-sharedDb'],
-      javaOpts: '',
-      installPath: 'dynamodb-local',
-      downloadUrl: 'https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz',
-    },
-  })) as { config: Config }
-
-  return config
+export const defaultConfig: Config = {
+  port: 8000,
+  defaultTableName: 'MyOfflineTable',
+  dbPath: '',
+  detached: false,
+  additionalArgs: ['-sharedDb'],
+  javaOpts: '',
+  installPath: 'dynamodb-local',
+  downloadUrl: 'https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz',
 }
 
-// Export the config (wrapped in a promise)
 // eslint-disable-next-line antfu/no-top-level-await
-export const config: Config = await loadDynamoDBConfig()
+export const config: Config = await loadConfig({
+  name: 'dynamodb',
+  cwd: resolve(__dirname, '..'),
+  defaultConfig,
+})
