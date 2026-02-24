ac47e49446d077146bdd142916bace6a2245d033
glennmichael123 <gtorregosa@gmail.com>
2026-02-24 18:10:44 +0800

    chore: wip

---
+++ src/shims.d.ts
@@ -0,0 +1,25 @@
+declare module 'cac' {
+  export class CAC {
+    constructor(name?: string)
+    command(name: string, description?: string): any
+    help(): this
+    version(version: string): this
+    parse(argv?: string[]): any
+    option(name: string, description?: string, config?: any): this
+  }
+  export function cac(name?: string): CAC
+  export default cac
+}
+
+declare module 'debug' {
+  interface Debug {
+    (formatter: string, ...args: any[]): void
+    enabled: boolean
+    namespace: string
+  }
+  interface DebugFactory {
+    (namespace: string): Debug
+  }
+  const debug: DebugFactory
+  export default debug
+}
